9dd97b70de96eb2fa17fbf1fb9dbfe8b5590b2ca
jeanmnorhen <jeanmdasleite@gmail.com>
2021-02-17 13:22:41 -0400

    nome

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Numeros</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 

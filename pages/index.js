40c155354affb73f917130d3e16beef636202757
Kabaloutz <criativo@kabalpublicidade.com.br>
2021-06-13 00:29:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    var today = new Date(),
-        date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-
-    this.state = {
-        date: date
-    };
+   
 
   return (
     <div className={styles.container}>

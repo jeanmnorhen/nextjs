3607c7b08b145616e65db5ddb440b2367a2ef950
Kabaloutz <criativo@kabalpublicidade.com.br>
2021-06-13 00:38:56 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title></title>
+        <title>{nome}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 

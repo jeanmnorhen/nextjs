3fa0ec1aab43bbe7ba3a468660b0dc8d15ba452e
Kabaloutz <criativo@kabalpublicidade.com.br>
2021-06-13 00:31:47 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  var today = new Date(),
+  date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+this.state = {
+  date: date
+};
+  return <Component {...pageProps,this.state} />
 }
 
 export default MyApp
